27d47b53f6f3ee09744f2991d3f8ecfbe778b5c8
alinapavlova1 <bigbrain012@mail.ru>
2023-09-17 05:01:40 +0300

    Добавлена реализация тестов функции check_ordinal_and_cardinal_pronouns

--- UnitTests/UnitTests.cpp
+++ UnitTests/UnitTests.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
 #include "CppUnitTest.h"
+#include "../e_p/e_p.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -9,8 +10,36 @@ namespace UnitTests
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(check_ordinal_and_cardinal_pronouns_ordinalNumberError)
 		{
+			std::string s = check_ordinal_and_cardinal_pronouns("first");
+			std::string expStr = "Неправильное употребление порядковых числительных. Должно быть  “first”.";
+
+			Assert::AreEqual(expStr, s);
+		}
+
+		TEST_METHOD(check_ordinal_and_cardinal_pronouns_cardinalNumberError)
+		{
+			std::string s = check_ordinal_and_cardinal_pronouns("one");
+			std::string expStr = "Неправильное употребление числительных. Должно быть  “one”.";
+
+			Assert::AreEqual(expStr, s);
+		}
+
+		TEST_METHOD(check_ordinal_and_cardinal_pronouns_ordinalComplexNumberError)
+		{
+			std::string s = check_ordinal_and_cardinal_pronouns("twenty-seventh");
+			std::string expStr = "Неправильное употребление порядковых числительных. Должно быть “twenty-seventh”.";
+
+			Assert::AreEqual(expStr, s);
+		}
+
+		TEST_METHOD(check_ordinal_and_cardinal_pronouns_noNumberError)
+		{
+			std::string s = check_ordinal_and_cardinal_pronouns("boxes");
+			std::string expStr = "";
+
+			Assert::AreEqual(expStr, s);
 		}
 	};
 }
