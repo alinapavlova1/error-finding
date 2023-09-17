4ec51251d901c925f0877f4a06a1008492df67c8
alinapavlova1 <bigbrain012@mail.ru>
2023-09-17 04:26:13 +0300

    Добавлены комментарии для автодокументации Doxygen

--- e_p/e_p.cpp
+++ e_p/e_p.cpp
@@ -5,7 +5,12 @@
 #include <string>
 #include <algorithm>
 
-// Функция разделения строки на слова по разделителю
+/*!
+* \brief Функция, которая выполняет разделение строки на слова по заданному разделителю 
+* \param[in] s - представляет собой исходную строку, которую нужно разделить на слова.
+* \param[in] delimeter - представляет собой разделитель, который будет использоваться для разделения строки на слова.
+* \return вектор строк, который содержит разделенные слова.
+*/
 std::vector<std::string> split(const std::string& s, char delimeter) {
     std::string word = "";
     std::vector<std::string> words;
@@ -25,7 +30,10 @@ std::vector<std::string> split(const std::string& s, char delimeter) {
 }
 
 
-// Функция валидирования количества входных аргументов командной строки
+/*!
+* \brief Функция валидирования количества входных аргументов командной строки
+* \param[in] argc - количество входных аргументов командной строки
+*/
 void validate_argument_count(int argc)
 {
     if (argc != 4)
@@ -36,7 +44,11 @@ void validate_argument_count(int argc)
 }
 
 
-// Функция, которая читает данные из файла
+/*!
+* \brief Функция, которая читает данные из файла
+* \param[in] file_name - путь к файлу чтения
+* \return read_data - поток считанных данных из файла
+*/
 void read_data(const std::string& file_name, std::string& read_data)
 {
     std::ifstream in (file_name);
@@ -64,7 +76,12 @@ void read_data(const std::string& file_name, std::string& read_data)
 }
 
 
-// Функция записи текста в выходной файл
+
+/*!
+* \brief Функция записи текста в выходной файл
+* \param[in] file_name - путь к файлу записи
+* \param[in] write_data - поток записываемых данных в файл
+*/
 void write_data(const std::string& file_name, const std::string& write_data) {
     std::ofstream out(file_name);
     if (!out.is_open())
@@ -77,7 +94,10 @@ void write_data(const std::string& file_name, const std::string& write_data) {
 }
 
 
-// Функция валидации количества слов во входном файле
+/*!
+* \brief Функция валидации количества слов во входном файле
+* \param[in] words - вектор слов файла
+*/
 void validate_words_count_in_file(const std::vector<std::string>& words)
 {
     if ((words.size() < 1) || (words.size() > 10))
@@ -89,7 +109,11 @@ void validate_words_count_in_file(const std::vector<std::string>& words)
 }
 
 
-// Функция сопоставления количества слов в двух входных файлах
+/*!
+* \brief Функция сопоставления количества слов в двух входных файлах
+* \param[in] words_1 - вектор слов первого файла
+* \param[in] words_2 - вектор слов второго файла
+*/
 void validate_incorrect_words_count(const std::vector<std::string>& words_1, const std::vector<std::string>& words_2) 
 {
     validate_words_count_in_file(words_1);
@@ -110,7 +134,11 @@ void validate_incorrect_words_count(const std::vector<std::string>& words_1, con
 }
 
 
-// Функция перевода строки в нижний регистр
+
+/*!
+* \brief Функция перевода строки в нижний регистр
+* \param[in] sentence - строка, которую нужно перевести в нижний регистр.
+*/
 void to_lower_string(std::string& sentence)
 {
     for (int i = 0; i < sentence.size(); ++i) {
@@ -119,7 +147,10 @@ void to_lower_string(std::string& sentence)
 }
 
 
-// Функция валидации строки на предмет того, что в строке есть символы английского алфавита
+/*!
+* \brief Функция валидации строки на предмет того, что в строке есть символы английского алфавита
+* \param[in] sentence - строка, которую нужно проверить на наличие символов английского алфавита.
+*/
 void validate_english_alphabet(const std::string& sentence) 
 {
     bool flag = false;
@@ -138,7 +169,13 @@ void validate_english_alphabet(const std::string& sentence)
 }
 
 
-// Дополнительная функция для "среза" элементов строки с нужного индекса и в нужном количестве
+/*!
+* \brief Дополнительная функция для "среза" элементов строки с нужного индекса и в нужном количестве
+* \param[in] word -  строка, из которой мы хотим извлечь символы
+* \param[in] offset - указывает на индекс символа в строке word, с которого начнется извлечение символов.
+* \param[in] count - параметр указывает, сколько символов нужно извлечь из строки
+* \return строка, содержащая извлеченные символы
+*/
 std::string get_needed_chars(const std::string& word, int offset, int count) 
 {
     std::string lastTwoChars;
@@ -150,7 +187,10 @@ std::string get_needed_chars(const std::string& word, int offset, int count)
 }
 
 
-// Функция проверки того, что ошибка заключалась в неправильном сокращении глаголов
+/*!
+* \brief Функция проверки того, что ошибка заключалась в неправильном сокращении глаголов
+* \param[in] word - слово, которое нужно проверить на наличие неправильных сокращений глаголов. 
+*/
 std::string check_abbreviations_in_word(const std::string& word) 
 {
     if (word.find('\'') != -1)
@@ -170,8 +210,10 @@ std::string check_abbreviations_in_word(const std::string& word)
     return "";
 }
 
-
-// Функция проверки того, что ошибка заключалась в неправильном употреблении количественных/порядковых числительных
+/*!
+* \brief Функция проверки того, что ошибка заключалась в неправильном употреблении количественных/порядковых числительных
+* \param[in] word - слово, которое нужно проверить на наличие неправильных употреблений количественных и порядковых числительных. 
+*/
 std::string check_ordinal_and_cardinal_pronouns(const std::string& word)
 {
     std::string ordinal_pronouns[3] = { "first", "second", "third" };
@@ -208,7 +250,12 @@ std::string check_ordinal_and_cardinal_pronouns(const std::string& word)
 }
 
 
-// Функция поиска типа ошибки, допущенного в предложении
+
+/*!
+* \brief Функция поиска типа ошибки, допущенного в предложении
+* \param[in] right_word - слово - эталон 
+* \param[in] right_word - слово, проверяющееся на ошибку 
+*/
 std::string check_simple_words(std::string& right_word, std::string& wrong_word) 
 {
     std::string articles[3] = { "a", "an", "the" };
@@ -286,7 +333,12 @@ std::string check_simple_words(std::string& right_word, std::string& wrong_word)
 }
 
 
-// Функция нахождение того слова, в котором есть ошибка
+//
+/*!
+* \brief  Функция нахождение того слова, в котором есть ошибка 
+* \param[in] words_without_errors - вектор слов строки эталона
+* \param[in] words_with_errors - вектор слов строки с ошибками
+*/
 std::string find_errors(std::vector<std::string> words_without_errors, std::vector<std::string> words_with_errors)
 {
     for (int i = 0; i < words_without_errors.size() - 1; ++i) {
